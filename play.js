b0f0bd6daf0728f4e82ff30635c9a7f10b323a90
Evelyn Bauer <evybauer@gmail.com>
2019-12-04 23:41:00 +0000

    update added

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const connect = require('./client');
 console.log('Connecting ...');
-connect.connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function(key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-  
+connect();
 
 setupInput();
