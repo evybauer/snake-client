10bce5227a8bf6f2aabe6f1223e42e4f190f97f4
Evelyn Bauer <evybauer@gmail.com>
2019-12-05 00:16:03 +0000

    connection added

--- input.js
+++ input.js
@@ -1,18 +1,38 @@
-const setupInput = function() {
+let connection;
 
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding("utf8");
-stdin.resume();
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+//   const handleUserInput = function(key) {
+//     if (key === "\u0003") {
+//       process.exit();
+//     }
+//   };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
+  } else {
+    if (key === "w") {
+      console.log("up");
+      connection.write("Move: up")
+    }
+    if (key === "a") {
+      console.log("left"); connection.write("Move: left")
+    }
+    if (key === "s") {
+      console.log("down"); connection.write("Move: down")
+    }
+    if (key === "d") {
+      console.log("right"); connection.write("Move: right")
+    }
   }
 };
-stdin.on("data", handleUserInput);
-return stdin;
-};
 
-setupInput();
-
-module.exports = {setupInput};
+module.exports = { setupInput };
